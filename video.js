14bcb7d12e0fc2b8544088ccfbc235962a4e45e1
phptuts <glaserpower@gmail.com>
2024-07-02 08:56:00 -0700

    21-Transcribe-Video-Speechmatics

--- video.js
+++ video.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const admin = require("firebase-admin");
+const { Speechmatics } = require("speechmatics");
 
 // Allows us to use the .env file.
 dotenv.config();
@@ -25,4 +26,47 @@ admin.initializeApp({
   storageBucket: "video-analyzer-751d2.appspot.com",
 });
 
-console.log("success");
+// Listens for videos that have recently been uploaded to firebase
+admin
+  .firestore()
+  .collection("videos")
+  .where("status", "==", "uploaded")
+  .onSnapshot(async (snapshot) => {
+    const docs = snapshot
+      .docChanges()
+      .filter((d) => d.type == "added")
+      .map((d) => d.doc);
+
+    for (let doc of docs) {
+      try {
+        await transcribeVideo(doc);
+      } catch (error) {
+        console.log(error);
+        await doc.ref.update({ status: "error_transcribed" });
+      }
+    }
+  });
+
+/**
+ * Download the video and transcribes it.
+ * @param {admin.firestore.QueryDocumentSnapshot} doc
+ */
+async function transcribeVideo(doc) {
+  const docData = doc.data();
+  const bucket = admin.storage().bucket();
+  const videoFile = bucket.file(docData["videoPath"]);
+
+  const sm = new Speechmatics(process.env.SPEECHMATICS_API_KEY);
+  const [fileContents] = await videoFile.download();
+  const inputFile = { data: new Blob([fileContents]), fileName: doc.id };
+
+  const transcribedText = await sm.batch.transcribe(
+    inputFile,
+    {
+      transcription_config: { language: "en" },
+    },
+    "text"
+  );
+
+  await doc.ref.update({ transcribedText, status: "transcribed" });
+}
